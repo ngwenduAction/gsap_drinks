f169e03ee6cc0003cfb665df3170253f90bf2102
NJ Gambu <gambungwendu@gmail.com>
2025-11-06 23:14:03 +0200

    Implement Contact section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Cocktails from "./Components/Cocktails.jsx";
 import About from "./Components/About.jsx";
 import Art from "./Components/Art.jsx";
 import Menu from "./Components/Menu.jsx";
+import Contact from "./Components/Contact.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -18,6 +19,7 @@ const App = () => {
             <About/>
             <Art/>
             <Menu/>
+            <Contact/>
         </main>
     );
 }
